706c3a46b2c125272ec4cb4ec907528f71ab233d
gari8 <hagari8810@icloud.com>
2023-01-28 11:43:58 +0900

    hotfix: resolve lint

--- database.go
+++ database.go
@@ -24,48 +24,48 @@ func NewDatabase(dsn string) (*gorm.DB, error) {
 	return db, nil
 }
 
-func seed(db *gorm.DB) error {
-	db.Create([]*model.Program{
-		{
-			ID:          "1",
-			Name:        "Program 1",
-			Description: "Description 1",
-			Image:       "Image 1",
-			ProgramType: 0,
-			PlaceID:     "1",
-		},
-		{
-			ID:          "2",
-			Name:        "Program 2",
-			Description: "Description 2",
-			Image:       "Image 2",
-			ProgramType: 1,
-			PlaceID:     "1",
-		},
-		{
-			ID:          "3",
-			Name:        "Program 3",
-			Description: "Description 3",
-			Image:       "Image 3",
-			ProgramType: 2,
-			PlaceID:     "2",
-		},
-	})
-	db.Create([]*model.Place{
-		{
-			ID:        "1",
-			Name:      "Place 1",
-			Latitude:  1.0,
-			Longitude: 1.0,
-			PlaceType: 0,
-		},
-		{
-			ID:        "2",
-			Name:      "Place 2",
-			Latitude:  2.0,
-			Longitude: 2.0,
-			PlaceType: 1,
-		},
-	})
-	return nil
-}
+//func seed(db *gorm.DB) error {
+//	db.Create([]*model.Program{
+//		{
+//			ID:          "1",
+//			Name:        "Program 1",
+//			Description: "Description 1",
+//			Image:       "Image 1",
+//			ProgramType: 0,
+//			PlaceID:     "1",
+//		},
+//		{
+//			ID:          "2",
+//			Name:        "Program 2",
+//			Description: "Description 2",
+//			Image:       "Image 2",
+//			ProgramType: 1,
+//			PlaceID:     "1",
+//		},
+//		{
+//			ID:          "3",
+//			Name:        "Program 3",
+//			Description: "Description 3",
+//			Image:       "Image 3",
+//			ProgramType: 2,
+//			PlaceID:     "2",
+//		},
+//	})
+//	db.Create([]*model.Place{
+//		{
+//			ID:        "1",
+//			Name:      "Place 1",
+//			Latitude:  1.0,
+//			Longitude: 1.0,
+//			PlaceType: 0,
+//		},
+//		{
+//			ID:        "2",
+//			Name:      "Place 2",
+//			Latitude:  2.0,
+//			Longitude: 2.0,
+//			PlaceType: 1,
+//		},
+//	})
+//	return nil
+//}
